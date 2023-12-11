ca83a484e0ae534bc573419e654925f8622f818e
kaybrian <kayonbrian@gmail.com>
2023-12-11 20:13:06 +0200

    fix the error on cloudinary

--- backend/CtrlApp/settings.py
+++ backend/CtrlApp/settings.py
@@ -224,7 +224,9 @@ EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 cloudinary.config(
     cloud_name=config("CLOUDINARY_CLOUD_NAME"),
     api_key=config("CLOUDINARY_API_KEY"),
-    api_secret=config("CLOUDINARY_API_SECRET")
+    api_secret=config("CLOUDINARY_API_SECRET"),
+    api_proxy='http://proxy.server:3128'
+
 )
 
 # swagger
